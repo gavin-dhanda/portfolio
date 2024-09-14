2421db6aa9f0c4ab5135cb0e4d596718ec8a5b7b
“gdhanda26” <gavin_dhanda@brown.edu>
2024-09-14 18:31:17 -0400

    project descriptions

--- components/proj.js
+++ components/proj.js
@@ -3,23 +3,24 @@ import styles from './experience.module.css';
 const ProjectBlock = ({ title, subtitle, description, imagelink}) => {
     return (
     <div className={styles.experience}>
-  
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "space-between", 
-      width: "100%", 
-    }}>
-    <div>
-    <h2 style={{ marginTop: "1em" }} className={styles.coolGradient}>{title}</h2>
-  <h3 style={{ margin: 0 }}>{subtitle}</h3>
-  {description}
+      <div
+        style={{
+        display: "flex",
+        justifyContent: "space-between", 
+        width: "100%", 
+        alignItems: "center",
+      }}>
+        <div style={{ flex : 1.2 }}>
+          <h2 style={{ marginTop: "1em" }} className={styles.coolGradient}>{title}</h2>
+          <h3 style={{ margin: 0 }}>{subtitle}</h3>
+          {description}
+        </div>
+        <div style={{ flex : 0.1 }}></div>
+        <span style={{ flex : 0.7 }}>
+          <img src={imagelink} alt="Project Image" className={styles.projectImage}></img>
+        </span>
+      </div>
     </div>
-  <span>
-    <img src={imagelink} alt="Project Image" className={styles.projectImage}></img>
-  </span>
-</div>
-</div>
 );}
 
 export default ProjectBlock;
