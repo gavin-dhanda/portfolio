860898afa5c7dea97856c9f945ad7eee2a7e71fa
Gavin Dhanda <143986735+gavin-dhanda@users.noreply.github.com>
2024-09-21 18:35:17 -0400

    project pages improved

--- components/proj_pages.js
+++ components/proj_pages.js
@@ -1,19 +1,29 @@
-// Create an image template for each project
 import styles from './projects.module.css';
 
 const ProjectImage = ({ src, alt, gitLink }) => {
+  const fileType = src.split(".").pop();
+
   return (
     <div>
         <div className={styles.linkBox}>
             <h5> <a href={"/"} >Home</a> </h5>
             <h5> <a href={"/projects"} >Back</a> </h5>
-            <h5> <a href={gitLink} target="_blank" rel="noreferrer">GitHub</a> </h5>
+
+            {gitLink !== "" && (
+            <h5> <a href={gitLink} target="_blank" rel="noreferrer">GitHub</a> </h5>)}
+
         </div>
-        <img src={src} alt={alt} className={styles.projImage}/>
+        {fileType === "jpg" && 
+        <img src={src} alt={alt} className={styles.projImage}/>}
+
+        {fileType === "mp4" && 
+        <video controls className={styles.projImage}>
+            <source src={src} type="video/mp4" />
+            Your browser does not support video.
+        </video>}
     </div>
   );
 }
-
 
 const ProjectPage = ({ src, alt, gitLink }) => {
   return (
