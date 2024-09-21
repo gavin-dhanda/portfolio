730febc1e7afc7eb43865f08e0d85d6b2731644b
Gavin Dhanda <143986735+gavin-dhanda@users.noreply.github.com>
2024-09-21 16:57:57 -0400

    updated project page structure

--- components/proj_pages.js
+++ components/proj_pages.js
@@ -4,11 +4,24 @@ import styles from './projects.module.css';
 const ProjectImage = ({ src, alt, gitLink }) => {
   return (
     <div>
-      <img src={src} alt={alt} className={styles.projImage}/>
-      <h5 style={{textAlign: 'center', marginTop: 0}}> 
-        <a href={gitLink} target="_blank" rel="noreferrer">GitHub Repository</a> </h5>
+        <div className={styles.linkBox}>
+            <h5> <a href={"/"} >Home</a> </h5>
+            <h5> <a href={"/projects"} >Back</a> </h5>
+            <h5> <a href={gitLink} target="_blank" rel="noreferrer">GitHub</a> </h5>
+        </div>
+        <img src={src} alt={alt} className={styles.projImage}/>
     </div>
   );
 }
 
-export default ProjectImage;
+
+const ProjectPage = ({ src, alt, gitLink }) => {
+  return (
+    <div>
+    <ProjectImage src={src} alt={alt} gitLink={gitLink} />
+    <h2 style={{marginTop: "0.5em"}}> Project Description </h2>
+    </div>
+  );
+}
+
+export default ProjectPage;
